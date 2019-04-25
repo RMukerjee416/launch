da3424ba9e03ca6fc50eaaccd80212082906ae97
Rajkumar <raj_muk@hotmail.com>
2019-04-25 05:27:51 +0000

    Attempt to establish validation error messages was unsuccessful

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,15 +7,17 @@ class QuotesController < ApplicationController
 		@quote = Quote.new
 	end
 
-def create
-	Quote.create(quote_params)
-	redirect_to root_path
-end
+	def create
+		@quote = Quote.create(quote_params)
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+		end
+		redirect_to root_path
+	end
 
-private
+	private
 
-def quote_params
-	params.require(:quote).permit(:saying, :author)
-end
-
+	def quote_params
+		params.require(:quote).permit(:saying, :author)
+	end
 end
